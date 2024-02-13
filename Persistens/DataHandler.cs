3736034d4eee5b12eb1e53640e40c219741bc017
Patrick <patrickhass84@gmail.com>
2024-02-13 11:15:33 +0100

    Add project files.

---
+++ Persistens/DataHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistens;
+
+namespace Persistens
+{
+    public class DataHandler
+    {
+        string dataFileName;
+        public string DataFileName { get { return dataFileName; } }
+        public DataHandler(string dataFileName) { this.dataFileName = dataFileName; }
+        public void SavePerson(Person person) 
+        {
+            using (StreamWriter writer = new StreamWriter(dataFileName))
+            {
+                writer.Write(person.MakeTitle());
+            }
+        }
+        public Person LoadPerson()
+        {
+            using (StreamReader reader = new StreamReader(dataFileName))
+            {
+                string loadedPerson = reader.ReadLine();
+
+            string[] splittedData = loadedPerson.Split(";");
+
+            Person p = new Person(splittedData[0], 
+                                DateTime.Parse(splittedData[1]), 
+                                double.Parse(splittedData[2]), 
+                                bool.Parse(splittedData[3]), 
+                                int.Parse(splittedData[4]));
+            
+            return p;
+            }
+
+            }
+        public void SavePersons(Person[] persons)
+        {
+            using (StreamWriter sw = new StreamWriter(dataFileName))
+            {
+                for (int i = 0; i < persons.Length; i++)
+                {
+
+                    if (persons != null)
+                    {
+                        sw.WriteLine($"{persons[i].Name};{persons[i].BirthDate};{persons[i].Height};{persons[i].IsMarried};{persons[i].NoOfChildren}");
+                    }
+                }
+            }
+        }
+
+        public Person[] LoadPersons()
+        {
+            using (StreamReader sr = new StreamReader(dataFileName))
+            {
+            String[] splittedLines = sr.ReadToEnd().Split("\n");
+            
+            Person[] personArray = new Person[splittedLines.Length];
+
+            for (int i = 0; i < personArray.Length - 1; i++)
+            {
+                String[] splitData = splittedLines[i].Split(";");
+
+                Person p = new Person(splitData[0], 
+                                        DateTime.Parse(splitData[1]), 
+                                        double.Parse(splitData[2]), 
+                                        bool.Parse(splitData[3]), 
+                                        int.Parse(splitData[4]));
+                personArray[i] = p;
+            }
+            return personArray;            
+            }
+        }
+    }
+}
---
+++ Persistens/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistens
+{
+    public class Person
+    {
+        string name;
+        DateTime birthDate;
+        double height;
+        bool isMarried;
+        int noOfChildren;
+        
+        public string Name { get { return name; } set { name = value; } }
+        public DateTime BirthDate 
+        {   get { return birthDate; } 
+            set { birthDate = value; }
+        }
+        public double Height { get { return height; } set { height = value; } }
+        public bool IsMarried { get { return isMarried; } set { isMarried = value; } }
+        public int NoOfChildren { get { return noOfChildren; } set { noOfChildren = value; } }
+
+
+        public Person(string name, DateTime birthDate, double height, bool isMarried, int noOfChildren) { 
+            this.name = name;
+            this.birthDate = birthDate;
+            this.height = height;
+            this.isMarried = isMarried;
+            this.noOfChildren = noOfChildren;
+        }
+
+        public string MakeTitle() {
+            string title = $"{name};{birthDate};{height};{isMarried};{noOfChildren}";
+            return title;
+        }
+    }
+}
